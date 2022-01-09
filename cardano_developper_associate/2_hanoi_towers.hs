1d32851e86f2d33b47f83ef60806855557400c25
Thomas Langlois <thomas.langlois@mac.com>
2022-01-09 18:07:07 +0000

    tweak

---
+++ cardano_developper_associate/2_hanoi_towers.hs
@@ -0,0 +1,8 @@
+-- Towers of Hanoi problem
+
+-- 3 poles are assumed
+-- Initially all disks are stacked on pole 1
+
+-- Given the number of disks
+-- List of all the steps that take the disks from the source pole to the target pole
+moveFromTo :: Int -> [[[Int]]]
